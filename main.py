21a0fce306f0d982ad1be4d98dbe5759b5c6c090
Caio Venancio <caiovenancio06@gmail.com>
2025-10-17 00:35:09 -0300

    feat: adicionar instancias de dados para iniciar na execucao

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from menu import menu
+from cidades.repository import CidadesDB
+from cursos.repository import CursosDB
+from alunos.repository import AlunosDB
+from autores.repository import AutoresDB
+from categorias.repository import CategoriasDB
+from livros.repository import LivrosDB
+from emprestimos.repository import EmprestimosDB
 
 def main():
-    menu()
+    cidades_db = CidadesDB()
+    cursos_db = CursosDB()
+    alunos_db = AlunosDB()
+    autores_db = AutoresDB()
+    categorias_db = CategoriasDB()
+    livros_db = LivrosDB()
+    emprestimos_db = EmprestimosDB()
+
+    menu(cidades_db, cursos_db, alunos_db, autores_db, categorias_db, livros_db, emprestimos_db)
 
 if __name__ == "__main__":
     main()
--- menu.py
+++ menu.py
@@ -8,7 +8,16 @@ from livros.menu import menu_livros
 from emprestimos.menu import menu_emprestimos
 from utils.ui import limpar_tela
 
-def menu():
+# Base de dados
+from cidades.menu import menu_cidades
+from cursos.menu import menu_cursos
+from alunos.menu import menu_alunos
+from autores.menu import menu_autores
+from categorias.menu import menu_categorias
+from livros.menu import menu_livros
+from emprestimos.menu import menu_emprestimos
+
+def menu(cidades_db, cursos_db, alunos_db, autores_db, categorias_db, livros_db, emprestimos_db):
     while True:
         limpar_tela()
         print("\n---- Sistema de Biblioteca ----\n")
@@ -23,22 +32,24 @@ def menu():
 
         escolha = input("Escolha: ").strip()
         if escolha == '1':
-            menu_cidades()
+            menu_cidades(cidades_db)
         elif escolha == '2':
-            menu_cursos()
+            menu_cursos(cursos_db)
         elif escolha == '3':
-            menu_alunos()
+            menu_alunos(alunos_db, cursos_db, cidades_db)
         elif escolha == '4':
-            menu_autores()
+            menu_autores(autores_db, cidades_db)
         elif escolha == '5':
-            menu_categorias()
+            menu_categorias(categorias_db)
         elif escolha == '6':
-            menu_livros()
+            menu_livros(livros_db, autores_db, categorias_db, cidades_db)
         elif escolha == '7':
-            menu_emprestimos()
+            menu_emprestimos(emprestimos_db, livros_db, alunos_db)
             
         elif escolha == '0':
             print("Saindo...\n")
             break
         else:
             print("Opção inválida.")
+
+        input("\nPressione Enter para continuar...")
